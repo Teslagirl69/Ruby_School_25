9e7f48ec5879d38fcec94d7b0a6f27cb5dfa6489
Teslagirl69 <teslagirl17@gmail.com>
2022-05-05 19:06:40 +0300

    Execute select query

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require 'sqlite3'
 db= SQLite3::Database.new 'test.sqlite'
-db.execute "INSERT 	INTO Cars (name, Price) VALUES ('Jaguar', 80000)"
+
+db.execute "SELECT * FROM Cars" do |car|
+puts car
+puts "======="
+end
+
 db.close
